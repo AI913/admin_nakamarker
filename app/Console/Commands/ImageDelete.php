68e7ae647850c22f37043e80d7eaa08568a7673d
toshi-work <toshiya.murasaki@frontarc.co.jp>
2020-10-01 11:44:07 +0900

    image:deleteコマンドに例外を追加

--- app/Console/Commands/ImageDelete.php
+++ app/Console/Commands/ImageDelete.php
@@ -39,35 +39,39 @@ class ImageDelete extends Command
      */
     public function handle()
     {
-        // 各フォルダに保管しているイメージ名を取得
-        $storage_markers = Storage::files('public/images/markers');
-        $storage_communities = Storage::files('public/images/communities');
-        $storage_community_locations = Storage::files('public/images/community_locations');
-        // $storage_user_locations = Storage::files('public/images/user_locations');
-        $storage_news = Storage::files('public/images/news');
+        try {
+            // 各フォルダに保管しているイメージ名を取得
+            $storage_markers = Storage::files('public/images/markers');
+            $storage_communities = Storage::files('public/images/communities');
+            $storage_community_locations = Storage::files('public/images/community_locations');
+            // $storage_user_locations = Storage::files('public/images/user_locations');
+            $storage_news = Storage::files('public/images/news');
 
-        // DBに存在しないファイル名のファイルがストレージにある場合は削除
-        foreach($storage_markers as $value) {
-            if(!DB::table('markers')->where('image_file', basename($value))->exists()) {
-                Storage::delete("public/images/markers/".basename($value));
+            // DBに存在しないファイル名のファイルがストレージにある場合は削除
+            foreach($storage_markers as $value) {
+                if(!DB::table('markers')->where('image_file', basename($value))->exists()) {
+                    Storage::delete("public/images/markers/".basename($value));
+                }
             }
-        }
-        foreach($storage_communities as $value) {
-            if(!DB::table('communities')->where('image_file', basename($value))->exists()) {
-                Storage::delete("public/images/communities/".basename($value));
+            foreach($storage_communities as $value) {
+                if(!DB::table('communities')->where('image_file', basename($value))->exists()) {
+                    Storage::delete("public/images/communities/".basename($value));
+                }
             }
-        }
-        foreach($storage_community_locations as $value) {
-            if(!DB::table('community_locations')->where('image_file', basename($value))->exists()) {
-                Storage::delete("public/images/community_locations/".basename($value));
+            foreach($storage_community_locations as $value) {
+                if(!DB::table('community_locations')->where('image_file', basename($value))->exists()) {
+                    Storage::delete("public/images/community_locations/".basename($value));
+                }
             }
-        }
-        foreach($storage_news as $value) {
-            if(!DB::table('news')->where('image_file', basename($value))->exists()) {
-                Storage::delete("public/images/news/".basename($value));
+            foreach($storage_news as $value) {
+                if(!DB::table('news')->where('image_file', basename($value))->exists()) {
+                    Storage::delete("public/images/news/".basename($value));
+                }
             }
-        }
+            logger()->info('All images delete Completed! .');
 
-        logger()->info('All images delete Completed! .');
+        } catch(\Exception $e) {
+            logger()->error($e->getMessage());
+        }
     }
 }
