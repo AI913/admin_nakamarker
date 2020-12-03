e2c76a818452b119e47639d80b93ccfdda6724a4
toshi-work <toshiya.murasaki@frontarc.co.jp>
2020-12-03 14:41:01 +0900

    ルーティングファイルの整備

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,27 @@
 use Illuminate\Http\Request;
 
 /************************************************
- *  アプリ側ルーティング(ログイン)
+ *  アプリ側ルーティング(非ログイン)
  ************************************************/
 Route::post('/user/create', 'Api\UserController@create');
 Route::post('/login', 'Api\UserController@login');
 
+/************************************************
+ *  アプリ側ルーティング(ログイン)
+ ************************************************/
 Route::middleware('app.auth')->group(function(){
+
+    /******************** ユーザ管理(users) ********************/
     Route::post('/user', 'Api\UserController@info');
-    Route::post('/user/point', 'Api\UserController@pointInfo');
-    Route::post('/user/point/update', 'Api\UserController@pointUpdate');
     Route::post('/user/update', 'Api\UserController@update');
     Route::post('/user/register', 'Api\UserController@register');
     Route::post('/password', 'Api\UserController@password');
+    /******************** ユーザポイント管理(user_points_histories) ********************/
+    Route::post('/user/point', 'Api\UserController@pointInfo');
+    Route::post('/user/point/update', 'Api\UserController@pointUpdate');
+    /******************** ユーザロケーション管理(user_locations) ********************/
+    Route::post('/user/location', 'Api\UserController@locationInfo');
+
+    /******************** 共通設定管理(configs) ********************/
     Route::post('/config', 'Api\ConfigController@index');
 });
