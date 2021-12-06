747fa2f0dab1580abec8b386968b0c4a19f544c0
takao.taniyama <takao.taniyama@frontarc.co.jp>
2021-12-06 19:52:21 +0900

    コミュニティ参加人数条件修正

--- app/Services/Model/CommunityService.php
+++ app/Services/Model/CommunityService.php
@@ -16,36 +16,37 @@ class CommunityService extends BaseService
         $this->model = $model;
     }
 
-    
+
     /**
      * ユーザ一覧ページに表示するコミュニティデータを取得
      * 引数:ユーザID
      */
     public function getUserCommunityQuery($user_id) {
         $query = $this->model()->query();
-    
+
         $query->leftJoin('community_histories', 'communities.id', 'community_histories.community_id')
               ->leftJoin('users', 'community_histories.user_id', 'users.id')
-              ->select('communities.*', 'users.id as user_id', 'community_histories.status as entry_status', 
+              ->select('communities.*', 'users.id as user_id', 'community_histories.status as entry_status',
                         'community_histories.memo', 'community_histories.id as community_history_id'
                 )
               ->where('community_histories.user_id', '=', $user_id)
               ->where('community_histories.del_flg', '=', 0);
-              
+
         return $query;
     }
 
     /**
      * コミュニティの参加人数を取得
-     * 
+     *
      */
     public function joinCount() {
         $query = $this->model()->query();
 
         $query->leftJoin('community_histories', 'communities.id', '=', 'community_histories.community_id')
-              ->selectRaw('count(community_histories.user_id) as total_counts')
-              ->addSelect('community_histories.community_id')
-              ->groupByRaw('community_histories.community_id');
+                ->selectRaw('count(community_histories.user_id) as total_counts')
+                ->addSelect('community_histories.community_id')
+                ->where('community_histories.del_flg', 0)
+                ->groupByRaw('community_histories.community_id');
 
         return $query;
     }
@@ -63,7 +64,7 @@ class CommunityService extends BaseService
               ->leftJoin('users', 'communities.host_user_id', '=', 'users.id')
               ->select('communities.*', 'j.total_counts', 'users.name as host_user_name')
               ->where('communities.del_flg', '=', 0);
-        
+
         // 検索条件があれば実行
         if($conditions) {
             $query = $this->getConditions($query, $this->model()->getTable(), $conditions);
