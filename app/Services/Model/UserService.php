732a42e70e9df9500fce6413664cdabc3b3840d6
takao.taniyama <takao.taniyama@frontarc.co.jp>
2021-12-06 19:33:45 +0900

    参加コミュニティのユーザー一覧。削除されているデータを除外する

--- app/Services/Model/UserService.php
+++ app/Services/Model/UserService.php
@@ -98,7 +98,7 @@ class UserService extends BaseService
 
         $query->leftJoin('user_markers', 'users.id', '=', 'user_markers.user_id')
               ->leftJoin('markers', 'user_markers.marker_id', '=', 'markers.id')
-              ->select('user_markers.id as user_markers_id', 'user_markers.updated_at as user_markers_updated_at', 
+              ->select('user_markers.id as user_markers_id', 'user_markers.updated_at as user_markers_updated_at',
                        'users.id as user_id', 'users.name as user_name', 'users.email as user_email', 'users.status'
                        )
               ->where('markers.id', '=', $marker_id)
@@ -110,23 +110,24 @@ class UserService extends BaseService
     /**
      * コミュニティに属しているユーザデータの取得
      * 引数1：コミュニティID, 引数2: ユーザID(一覧を表示する場合は要省略)
-     * 
+     *
      */
     public function getCommunityUserQuery($community_id, $user_id=null) {
         $query = $this->model()->query();
-    
+
         $query->leftJoin('community_histories', 'users.id', 'community_histories.user_id')
               ->leftJoin('communities', 'communities.id', 'community_histories.community_id')
-              ->select('users.*', 'communities.id as community_id', 'community_histories.status as entry_status', 
+              ->select('users.*', 'communities.id as community_id', 'community_histories.status as entry_status',
                        'community_histories.memo as entry_memo', 'community_histories.id as history_id')
-              ->where('community_id', '=', $community_id)
+            ->where('community_id', '=', $community_id)
+            ->where('community_histories', '=', 0)
               ->where('users.del_flg', '=', 0);
 
         // ユーザ情報の詳細を取得する際に設定
         if(!is_null($user_id)) {
             $query->where('users.id', '=', $user_id);
         }
-              
+
         return $query;
     }
 
